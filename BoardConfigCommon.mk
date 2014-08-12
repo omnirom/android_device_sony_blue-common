b1aed2d2204a7818357c85fcde3e3d93fc2a4f67
Hamster Tian <th0928@126.com>
2014-08-12 09:46:28 +0800

    blue-common: support proper brightness adjustment in Recovery
    Use with https://gerrit.omnirom.org/#/c/8662/6 . This is
    merged now.
    
    Change-Id: I196210095875b27d34de5ab3208529767dba22d8

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -157,7 +157,8 @@ TW_CRYPTO_FS_FLAGS := "0x00000406"
 TW_CRYPTO_KEY_LOC := "footer"
 TW_INCLUDE_FUSE_EXFAT := true
 TW_MAX_BRIGHTNESS := 255
-TW_NO_USB_STORAGE := true
+TW_BRIGHTNESS_PATH := "/sys/class/leds/lcd-backlight_1/brightness"
+TW_SECONDARY_BRIGHTNESS_PATH := "/sys/class/leds/lcd-backlight_2/brightness"
 
 BOARD_SEPOLICY_DIRS += \
     device/sony/blue-common/sepolicy
